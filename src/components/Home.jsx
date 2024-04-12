88e30df973e2a43e25f849d06ccc5d927412723c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-11 21:40:08 -0300

    add home title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,9 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center mt-6">
       <span className="text-3xl font-barlow text-white uppercase tracking-widest">so, you want to travel to</span>
+      <h1 className="text-9xl font-bellefair text-white uppercase mt-8">space</h1>
     </div>
   );
 }
