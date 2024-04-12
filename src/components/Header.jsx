256d7eaeec953bdf77f0477b75084558970d7d8f
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-11 21:17:10 -0300

    add header icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.css';
 
+import logo from '../assets/icons/logo.svg'
+import hamburguer from '../assets/icons/icon-hamburger.svg'
+
 function Header() {
   return (
     <div className="Header">
-      Header
+      <img src={logo} alt="logo" />
+      <img src={hamburguer} alt="hamburguer" />
     </div>
   );
 }
