a972ca6fc7c92290d17cf9101795eb92b59902ea
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-12 20:53:11 -0300

    add side Menu position and background

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ import './Menu.css';
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <div className="absolute top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">Menu</div>
   );
 }
 
