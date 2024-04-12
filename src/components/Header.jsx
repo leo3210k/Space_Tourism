ca946b3a3557ebeda64cc084972c8fe4273f88c5
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-12 20:52:11 -0300

    add Menu component to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import './Header.css';
 import logo from '../assets/icons/logo.svg'
 import hamburguer from '../assets/icons/icon-hamburger.svg'
 
+import Menu from './Menu';
+
 function Header() {
   return (
-    <header className="flex justify-between items-center p-10">
+    <header className="relative flex justify-between items-center p-10">
       <img src={logo} alt="logo" />
       <img src={hamburguer} alt="hamburguer" />
+      <Menu />
     </header>
   );
 }
