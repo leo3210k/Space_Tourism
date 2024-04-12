e77c6702df6dc7c5bb066338c92fad3b63abb272
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-11 22:24:28 -0300

    add width to home body

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,14 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className="flex flex-col items-center mt-6">
+    <div className=" flex flex-col items-center mt-6">
       <span className="text-3xl font-barlow text-white uppercase tracking-widest">so, you want to travel to</span>
       <h1 className="text-9xl font-bellefair text-white uppercase mt-8">space</h1>
+      <span className="w-4/6 text-lg text-white font-barlow line-clamp-5 text-center mt-6">
+        Let’s face it; if you want to go to space, you might as well genuinely go to outer space 
+        and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a 
+        truly out of this world experience!
+      </span>
     </div>
   );
 }
