b322f9a71b61b36729deedf9928b5d42d3e7d1ef
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-11 21:18:06 -0300

    change div to header and rename classname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ import hamburguer from '../assets/icons/icon-hamburger.svg'
 
 function Header() {
   return (
-    <div className="Header">
+    <header className="header">
       <img src={logo} alt="logo" />
       <img src={hamburguer} alt="hamburguer" />
-    </div>
+    </header>
   );
 }
 
