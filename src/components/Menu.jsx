8d93ed849824ad7802473dec9b98b56a22dcc6a5
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-13 18:14:27 -0300

    add bold to side menu numeration

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,19 +5,19 @@ function Menu() {
     <div className="absolute flex flex-col top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">
       <ol className="font-barlowX">
         <li className="flex gap-4 text-base uppercase">
-          <div>00</div>
+          <div className="font-bold">00</div>
           <span>home</span>
         </li>
         <li className="flex gap-4 text-base uppercase">
-          <div>01</div>
+          <div className="font-bold">01</div>
           <span>destination</span>
         </li>
         <li className="flex gap-4 text-base uppercase">
-          <div>02</div>
+          <div className="font-bold">02</div>
           <span>crew</span>
         </li>
         <li className="flex gap-4 text-base uppercase">
-          <div>03</div>
+          <div className="font-bold">03</div>
           <span>technology</span>
         </li>
       </ol>
