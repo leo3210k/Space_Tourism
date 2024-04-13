14df8305e5fe5c600534394197e99efef17a3487
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-13 18:14:00 -0300

    add list item style

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,20 +3,20 @@ import './Menu.css';
 function Menu() {
   return (
     <div className="absolute flex flex-col top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">
-      <ol>
-        <li>
+      <ol className="font-barlowX">
+        <li className="flex gap-4 text-base uppercase">
           <div>00</div>
           <span>home</span>
         </li>
-        <li>
+        <li className="flex gap-4 text-base uppercase">
           <div>01</div>
           <span>destination</span>
         </li>
-        <li>
+        <li className="flex gap-4 text-base uppercase">
           <div>02</div>
           <span>crew</span>
         </li>
-        <li>
+        <li className="flex gap-4 text-base uppercase">
           <div>03</div>
           <span>technology</span>
         </li>
