9f7fe5ec9276e1bad4be704316e2abac4395c381
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-13 18:13:30 -0300

    add ordered list to side menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,26 @@ import './Menu.css';
 
 function Menu() {
   return (
-    <div className="absolute top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">Menu</div>
+    <div className="absolute flex flex-col top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">
+      <ol>
+        <li>
+          <div>00</div>
+          <span>home</span>
+        </li>
+        <li>
+          <div>01</div>
+          <span>destination</span>
+        </li>
+        <li>
+          <div>02</div>
+          <span>crew</span>
+        </li>
+        <li>
+          <div>03</div>
+          <span>technology</span>
+        </li>
+      </ol>
+    </div>
   );
 }
 
