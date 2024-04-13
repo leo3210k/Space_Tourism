cd8eaae08e130583e3235a0ea67d8c4875834412
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-13 18:15:55 -0300

    add flex col and gap to side menu ordered list

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,8 +2,8 @@ import './Menu.css';
 
 function Menu() {
   return (
-    <div className="absolute flex flex-col top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">
-      <ol className="font-barlowX mt-40 ml-10">
+    <div className="absolute top-0 right-0 w-7/12 h-svh text-white backdrop-blur-2xl z-20">
+      <ol className="font-barlowX flex flex-col gap-8 mt-40 ml-10">
         <li className="flex gap-4 text-xl font-light uppercase">
           <div className="font-bold">00</div>
           <span>home</span>
