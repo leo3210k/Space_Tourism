bc6bccec4705baa1a80facacca37ac66016e4828
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-20 16:49:47 -0300

    remove comment from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,6 @@ function Header() {
   return (
     <header className="relative flex justify-between items-center p-10">
       <img src={logo} alt="logo" />
-      {/* <img src={hamburguer} className={`z-40 ${isSideMenuOpen ? 'active' : ''}`} alt="hamburguer" /> */}
       <MenuToggle isSideMenuOpen={isSideMenuOpen} toggle={() => setIsSideMenuOpen(!isSideMenuOpen)} />
       <Menu isSideMenuOpen={isSideMenuOpen}/>
     </header>
