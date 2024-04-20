ee60edc72beaba794364c66ce495231274ed3692
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-20 16:42:10 -0300

    remove comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,18 @@ import logo from '../assets/icons/logo.svg'
 import hamburguer from '../assets/icons/icon-hamburger.svg'
 
 import Menu from './Menu';
+import { useEffect, useState } from 'react';
+import { MenuToggle } from './utils/MenuToggle';
 
 function Header() {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
   return (
     <header className="relative flex justify-between items-center p-10">
       <img src={logo} alt="logo" />
-      <img src={hamburguer} alt="hamburguer" />
-      <Menu />
+      {/* <img src={hamburguer} className={`z-40 ${isSideMenuOpen ? 'active' : ''}`} alt="hamburguer" /> */}
+      <MenuToggle isSideMenuOpen toggle={() => setIsSideMenuOpen(!isSideMenuOpen)} />
+      {/* <Menu /> */}
     </header>
   );
 }
