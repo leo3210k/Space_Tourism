74aeb0105d77a1f5556fd36df6b61a4ccb8b73df
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-21 16:54:08 -0300

    add link to home on Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 import './Menu.css';
 
 function Menu({ isSideMenuOpen }) {
   return (
     <div className={`menu absolute top-0 right-0 ${isSideMenuOpen ? 'w-7/12' : 'w-0'} h-svh text-white backdrop-blur-2xl transition-all duration-700 ease-in z-20`}>
       <ol className="font-barlow flex flex-col gap-8 mt-40 ml-10">
-        <li className="flex gap-4 text-2xl font-light tracking-widest uppercase">
+        <Link to={"/"} className="flex gap-4 text-2xl font-light tracking-widest uppercase">
           <div className="font-bold">00</div>
           <span>home</span>
-        </li>
+        </Link>
         <Link to={"/destination"} className="flex gap-4 text-2xl font-light tracking-widest uppercase">
           <div className="font-bold">01</div>
           <span>destination</span>
