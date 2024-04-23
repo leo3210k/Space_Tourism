6f0c2b93da5315670e63f6bdd8bff64a78a069f8
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-23 15:48:57 -0300

    add method to change background image

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,10 +2,21 @@ import './App.css';
 
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  let location = useLocation();
+
+  const currentPath = function(location) {
+    if(location.pathname === '/') {
+      return 'home-mobile'
+    } else if(location.pathname === '/destination') {
+      return 'destination-mobile'
+    }
+  }
+
   return (
-    <div className="app">
+    <div className={`app bg-${currentPath(location)}`}>
       <Header />
       <Outlet />
     </div>
