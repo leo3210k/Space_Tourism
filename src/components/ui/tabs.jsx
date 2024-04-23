d0f263477abbed728e85daa63dee80d08aeacc87
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:03:11 -0300

    add mark to tab focused

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -22,7 +22,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-lg font-medium transition-all data-[state=active]:text-white",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-lg font-medium transition-all data-[state=active]:text-white data-[state=active]:after:content-['*'] data-[state=active]:after:absolute data-[state=active]:after:w-full data-[state=active]:after:h-4 data-[state=active]:after:bg-white",
       className
     )}
     {...props} />
