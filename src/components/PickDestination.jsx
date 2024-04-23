caa6320e312fab0b9162b00d2c70254d46481a73
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-23 16:18:24 -0300

    add europa content

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -60,11 +60,11 @@ function PickDestination() {
           travel_days="9 MONTHS"
           />
           <DestinationsContent value="europa" title="MOON" description="
-          See our planet as you’ve never seen it before. A perfect relaxing trip 
-          away to help regain perspective and come back refreshed. While you’re there, 
-          take in some history by visiting the Luna 2 and Apollo 11 landing sites."
-          distance="384,400 KM"
-          travel_days="3 DAYS"
+          The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter 
+          lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, 
+          curling, hockey, or simple relaxation in your snug wintery cabin."
+          distance="628 MIL. km"
+          travel_days="3 YEARS"
           />
           <DestinationsContent value="titan" title="MOON" description="
           See our planet as you’ve never seen it before. A perfect relaxing trip 
