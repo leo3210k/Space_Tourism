bf4b91c204371fcb008d89c2da5a2dcf18d1dcf1
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:03:57 -0300

    add gap to tabs

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -11,7 +11,7 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center text-tropical_blue",
+      "inline-flex h-10 items-center justify-center gap-6 text-tropical_blue",
       className
     )}
     {...props} />
