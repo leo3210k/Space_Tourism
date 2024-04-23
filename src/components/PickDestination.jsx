a55b09d0b73e0c5eab089a6317853b5aad02df1b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:08:28 -0300

    add margin to tabs list

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -13,11 +13,11 @@ function PickDestination() {
         <span className='text-2xl font-barlow text-white uppercase tracking-widest'>pick your destination</span>
       </div>
       <Tabs defaultValue="moon" className="w-[400px] flex flex-col items-center text-white">
-        <TabsContent value="moon"><img src={moon} className='w-40 h-40' alt="" /></TabsContent>
-        <TabsContent value="mars"><img src={mars} className='w-40 h-40' alt="" /></TabsContent>
-        <TabsContent value="europa"><img src={europa} className='w-40 h-40' alt="" /></TabsContent>
-        <TabsContent value="titan"><img src={titan} className='w-40 h-40' alt="" /></TabsContent>
-        <TabsList className="font-barlow">
+        <TabsContent value="moon"><img src={moon} className='w-40 h-40' alt="moon" /></TabsContent>
+        <TabsContent value="mars"><img src={mars} className='w-40 h-40' alt="mars" /></TabsContent>
+        <TabsContent value="europa"><img src={europa} className='w-40 h-40' alt="europa" /></TabsContent>
+        <TabsContent value="titan"><img src={titan} className='w-40 h-40' alt="titan" /></TabsContent>
+        <TabsList className="font-barlow my-6">
           <TabsTrigger value="moon">MOON</TabsTrigger>
           <TabsTrigger value="mars">MARS</TabsTrigger>
           <TabsTrigger value="europa">EUROPA</TabsTrigger>
