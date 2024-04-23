923d9c02700fb6a93e09edf5cb9f24de7ec9edeb
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:07:51 -0300

    import and add destination images

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -1,5 +1,9 @@
 import './PickDestination.css';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
+import moon from '../assets/destination/image-moon.png'
+import mars from '../assets/destination/image-mars.png'
+import europa from '../assets/destination/image-europa.png'
+import titan from '../assets/destination/image-titan.png'
 
 function PickDestination() {
   return (
@@ -8,21 +12,21 @@ function PickDestination() {
         <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>01</span>
         <span className='text-2xl font-barlow text-white uppercase tracking-widest'>pick your destination</span>
       </div>
-      <Tabs defaultValue="account" className="w-[400px] flex flex-col items-center text-white">
-        <TabsContent value="moon">Make changes to your moon here.</TabsContent>
-        <TabsContent value="mars">Change your password here.</TabsContent>
-        <TabsContent value="europa">Change your password here.</TabsContent>
-        <TabsContent value="titan">Change your password here.</TabsContent>
+      <Tabs defaultValue="moon" className="w-[400px] flex flex-col items-center text-white">
+        <TabsContent value="moon"><img src={moon} className='w-40 h-40' alt="" /></TabsContent>
+        <TabsContent value="mars"><img src={mars} className='w-40 h-40' alt="" /></TabsContent>
+        <TabsContent value="europa"><img src={europa} className='w-40 h-40' alt="" /></TabsContent>
+        <TabsContent value="titan"><img src={titan} className='w-40 h-40' alt="" /></TabsContent>
         <TabsList className="font-barlow">
           <TabsTrigger value="moon">MOON</TabsTrigger>
           <TabsTrigger value="mars">MARS</TabsTrigger>
           <TabsTrigger value="europa">EUROPA</TabsTrigger>
           <TabsTrigger value="titan">TITAN</TabsTrigger>
         </TabsList>
-        <TabsContent value="moon">Make changes to your moon here.</TabsContent>
-        <TabsContent value="mars">Change your password here.</TabsContent>
-        <TabsContent value="europa">Change your password here.</TabsContent>
-        <TabsContent value="titan">Change your password here.</TabsContent>
+        <TabsContent value="moon">Oláaaaaaaaaaaaaaaaaaaaaaaaaa</TabsContent>
+        <TabsContent value="mars"></TabsContent>
+        <TabsContent value="europa"></TabsContent>
+        <TabsContent value="titan"></TabsContent>
       </Tabs>
     </div>
   );
