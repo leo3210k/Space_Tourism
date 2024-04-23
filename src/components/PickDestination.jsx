77a367e53542c2197363c155e867ac96a1ea059b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:05:14 -0300

    add pick destination title

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -2,7 +2,12 @@ import './PickDestination.css';
 
 function PickDestination() {
   return (
-    <div>Pick Destination</div>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex gap-5'>
+        <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>01</span>
+        <span className='text-2xl font-barlow text-white uppercase tracking-widest'>pick your destination</span>
+      </div>
+    </div>
   );
 }
 
