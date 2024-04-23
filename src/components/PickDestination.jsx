0a4c6a651c6870de18c2d9c77a9222f1985da54a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-23 16:21:11 -0300

    change km distance to uppercase

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -56,21 +56,21 @@ function PickDestination() {
           Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus 
           Mons, the tallest planetary mountain in our solar system. It’s two and a half 
           times the size of Everest!"
-          distance="225 MIL. km"
+          distance="225 MIL. KM"
           travel_days="9 MONTHS"
           />
           <DestinationsContent value="europa" title="EUROPA" description="
           The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter 
           lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, 
           curling, hockey, or simple relaxation in your snug wintery cabin."
-          distance="628 MIL. km"
+          distance="628 MIL. KM"
           travel_days="3 YEARS"
           />
           <DestinationsContent value="titan" title="TITAN" description="
           The only moon known to have a dense atmosphere other than Earth, Titan is a 
           home away from home (just a few hundred degrees colder!). As a bonus, you get 
           striking views of the Rings of Saturn."
-          distance="1.6 BIL. km"
+          distance="1.6 BIL. KM"
           travel_days="7 YEARS"
           />
       </Tabs>
