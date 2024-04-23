55f16ff156390c2612cc47e40620dc27363a7183
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-23 16:18:08 -0300

    add mars content

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -52,12 +52,12 @@ function PickDestination() {
           distance="384,400 KM"
           travel_days="3 DAYS"
           />
-          <DestinationsContent value="mars" title="MOON" description="
-          See our planet as you’ve never seen it before. A perfect relaxing trip 
-          away to help regain perspective and come back refreshed. While you’re there, 
-          take in some history by visiting the Luna 2 and Apollo 11 landing sites."
-          distance="384,400 KM"
-          travel_days="3 DAYS"
+          <DestinationsContent value="mars" title="MARS" description="
+          Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus 
+          Mons, the tallest planetary mountain in our solar system. It’s two and a half 
+          times the size of Everest!"
+          distance="225 MIL. km"
+          travel_days="9 MONTHS"
           />
           <DestinationsContent value="europa" title="MOON" description="
           See our planet as you’ve never seen it before. A perfect relaxing trip 
