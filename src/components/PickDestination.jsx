c65fbb1b0591b971b40ae2208afd508ae9c85746
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:05:39 -0300

    add shadcn tabs on pick destination

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -1,4 +1,5 @@
 import './PickDestination.css';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function PickDestination() {
   return (
@@ -7,6 +8,14 @@ function PickDestination() {
         <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>01</span>
         <span className='text-2xl font-barlow text-white uppercase tracking-widest'>pick your destination</span>
       </div>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
     </div>
   );
 }
