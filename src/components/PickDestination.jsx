c8108cf7b5f015b1131958c20dc8db0e76fb53ac
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:08:07 -0300

    add gap to destinations page

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -7,7 +7,7 @@ import titan from '../assets/destination/image-titan.png'
 
 function PickDestination() {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center gap-6'>
       <div className='flex gap-5'>
         <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>01</span>
         <span className='text-2xl font-barlow text-white uppercase tracking-widest'>pick your destination</span>
