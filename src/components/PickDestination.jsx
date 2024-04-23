d8332105ad1a0035f25065ea809b2d6126622d74
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-22 22:07:16 -0300

    add tabs values

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -8,13 +8,21 @@ function PickDestination() {
         <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>01</span>
         <span className='text-2xl font-barlow text-white uppercase tracking-widest'>pick your destination</span>
       </div>
-      <Tabs defaultValue="account" className="w-[400px] flex flex-col items-center">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+      <Tabs defaultValue="account" className="w-[400px] flex flex-col items-center text-white">
+        <TabsContent value="moon">Make changes to your moon here.</TabsContent>
+        <TabsContent value="mars">Change your password here.</TabsContent>
+        <TabsContent value="europa">Change your password here.</TabsContent>
+        <TabsContent value="titan">Change your password here.</TabsContent>
+        <TabsList className="font-barlow">
+          <TabsTrigger value="moon">MOON</TabsTrigger>
+          <TabsTrigger value="mars">MARS</TabsTrigger>
+          <TabsTrigger value="europa">EUROPA</TabsTrigger>
+          <TabsTrigger value="titan">TITAN</TabsTrigger>
         </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
+        <TabsContent value="moon">Make changes to your moon here.</TabsContent>
+        <TabsContent value="mars">Change your password here.</TabsContent>
+        <TabsContent value="europa">Change your password here.</TabsContent>
+        <TabsContent value="titan">Change your password here.</TabsContent>
       </Tabs>
     </div>
   );
