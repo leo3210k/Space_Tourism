f91c45ac24c45ecb7b8ae62a43c9e264c1570c54
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-23 21:12:27 -0300

    add embla carousel to crew page

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,21 +1,20 @@
 import './Crew.css';
+import EmblaCarousel from './utils/EmblaCarousel';
+
+import './utils/css/embla.css'
 
 function Crew() {
+  const OPTIONS = {}
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
   return (
-    <div className=" flex flex-col items-center mt-6">
-      <span className="text-3xl font-barlow text-white uppercase tracking-widest">so, you want to travel to</span>
-      <h1 className="text-9xl font-bellefair text-white uppercase mt-8">space</h1>
-      <span className="w-4/6 text-lg text-white font-barlow line-clamp-5 text-center mt-6">
-        Let’s face it; if you want to go to space, you might as well genuinely go to outer space 
-        and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a 
-        truly out of this world experience!
-      </span>
-      <div className="relative group cursor-pointer mt-24">
-        <div className="absolute w-48 h-48 bg-dusty_gray opacity-20 rounded-full transition-all duration-1000 group-hover:scale-150 z-0"></div>
-        <div className="relative w-48 h-48 flex justify-center items-center text-xl text-vulcan bg-white rounded-full font-bellefair uppercase z-10">
-          explore
-        </div>
+    <div className='flex flex-col justify-center items-center gap-6 -mt-4'>
+      <div className='flex gap-5'>
+        <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>02</span>
+        <span className='text-2xl font-barlow text-white uppercase tracking-widest'>meet your crew</span>
       </div>
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </div>
   );
 }
