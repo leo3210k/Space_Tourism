096ca42c25e45e836a96d53d350c8df394691ccc
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-25 20:22:25 -0300

    add crew informations

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,20 +1,42 @@
 import './Crew.css';
 import EmblaCarousel from './utils/EmblaCarousel';
 
-import './utils/css/embla.css'
-
 function Crew() {
-  const OPTIONS = {}
-  const SLIDE_COUNT = 5
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+  const OPTIONS = { loop: true }
+
+  const crew = [
+    {
+      src: '/crew/image-douglas-hurley.png',
+      title: 'Commander',
+      name: 'Douglas Hurley',
+      description: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.'
+    },
+    {
+      src: '/crew/image-mark-shuttleworth.png',
+      title: 'Mission Specialist ',
+      name: 'Mark Shuttleworth',
+      description: 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
+    },
+    {
+      src: '/crew/image-victor-glover.png',
+      title: 'Pilot',
+      name: 'Victor Glover',
+      description: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an Fe was a crew member of Expedition 64, and served as a station systems flight engineer. '
+    },
+    {
+      src: '/crew/image-anousheh-ansari.png',
+      title: 'Flight Engineer',
+      name: 'Anousheh Ansari',
+      description: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. '
+    }
+  ]
 
   return (
-    <div className='flex flex-col justify-center items-center gap-6 -mt-4'>
+    <div className='flex flex-col justify-center items-center gap-8 -mt-4'>
       <div className='flex gap-5'>
         <span className='text-2xl font-barlow font-bold text-dusty_gray tracking-widest opacity-50'>02</span>
         <span className='text-2xl font-barlow text-white uppercase tracking-widest'>meet your crew</span>
       </div>
-      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </div>
   );
 }
