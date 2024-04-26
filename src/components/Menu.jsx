a0454d64429e9aaa4a6fdb048bf6cc57e3e90c16
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-26 17:11:57 -0300

    change menu width

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,7 @@ import './Menu.css';
 
 function Menu({ isSideMenuOpen }) {
   return (
-    <div className={`menu absolute top-0 right-0 ${isSideMenuOpen ? 'w-7/12' : 'w-0'} h-svh text-white backdrop-blur-2xl transition-all duration-700 z-20`}>
+    <div className={`menu absolute top-0 right-0 ${isSideMenuOpen ? 'w-9/12' : 'w-0'} h-svh text-white backdrop-blur-2xl transition-all duration-700 z-20`}>
       <ol className="font-barlow flex flex-col gap-8 mt-40 ml-10">
         <Link to={"/"} className="flex gap-4 text-2xl font-light tracking-widest uppercase">
           <div className="font-bold">00</div>
