765c50ed38c32e7fd4f495b7446eb3582a259ede
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-26 16:46:41 -0300

    import styles from technology carousel module css

--- src/components/utils/TechnologyCarousel.jsx
+++ src/components/utils/TechnologyCarousel.jsx
@@ -3,7 +3,7 @@ import { DotButton, useDotButton } from './EmblaCarouselDotButton'
 import Autoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
 import './css/base.css'
-import './css/TechnologyCarousel.module.css'
+import styles from './TechnologyCarousel.module.css'
 
 const EmblaCarousel = (props) => {
   const { slides, options } = props
