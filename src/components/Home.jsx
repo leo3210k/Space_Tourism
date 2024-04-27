83d4a11766b51b887d8844115977db9ef60163c8
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-04-27 13:30:45 -0300

    change font size and margin on Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,10 @@ import './Home.css';
 
 function Home() {
   return (
-    <div className=" flex flex-col items-center mt-6">
-      <span className="text-3xl font-barlow text-white uppercase tracking-widest">so, you want to travel to</span>
-      <h1 className="text-9xl font-bellefair text-white uppercase mt-8">space</h1>
-      <span className="w-4/6 text-lg text-white font-barlow line-clamp-5 text-center mt-6">
+    <div className=" flex flex-col items-center mt-16">
+      <span className="text-base font-barlow text-white uppercase tracking-widest">so, you want to travel to</span>
+      <h1 className="text-8xl font-bellefair text-white uppercase mt-8">space</h1>
+      <span className="w-5/6 text-base text-tropical_blue font-barlow line-clamp-5 text-center leading-7 mt-8">
         Let’s face it; if you want to go to space, you might as well genuinely go to outer space 
         and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a 
         truly out of this world experience!
