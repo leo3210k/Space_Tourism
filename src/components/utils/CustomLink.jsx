beac910527fae24dc9039aa5d76515d3dcb463f4
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-01 17:38:11 -0300

    add underline effect on menu links

--- src/components/utils/CustomLink.jsx
+++ src/components/utils/CustomLink.jsx
@@ -4,7 +4,17 @@ import { Link } from 'react-router-dom';
 function CustomLink({ path, name }) {
   return (
     <Link to={path} className="relative">
-      <li>{name}</li>
+      <li>
+        {name}
+        <span className={`
+        h-[1px] inline-block
+        absolute left-0 -bottom-0.5 bg-${background}
+        group-hover:w-full transition-[width] ease duration-300
+        underline-offset-4
+        ${router.asPath === href ? "w-full" : "w-0"}
+        `}
+        >&nbsp;</span>
+      </li>
     </Link>
   );
 }
