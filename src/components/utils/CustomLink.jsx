90c37d50dfdc2f20a64d16810368d73ebe2ca4de
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-01 17:38:32 -0300

    remove not utilized background variable

--- src/components/utils/CustomLink.jsx
+++ src/components/utils/CustomLink.jsx
@@ -1,14 +1,16 @@
 import './CustomLink.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function CustomLink({ path, name }) {
+  let location = useLocation();
+
   return (
     <Link to={path} className="relative">
       <li>
         {name}
         <span className={`
         h-[1px] inline-block
-        absolute left-0 -bottom-0.5 bg-${background}
+        absolute left-0 -bottom-0.5
         group-hover:w-full transition-[width] ease duration-300
         underline-offset-4
         ${router.asPath === href ? "w-full" : "w-0"}
