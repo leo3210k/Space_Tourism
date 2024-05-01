7fe50cfba08ca2c8023d3ce9f2aa1b55fdf5abe1
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-01 16:46:27 -0300

    change subtitle color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css';
 function Home() {
   return (
     <div className="flex flex-col items-center mt-16">
-      <span className="text-base font-barlow text-white uppercase tracking-widest">so, you want to travel to</span>
+      <span className="text-base font-barlow text-tropical_blue uppercase tracking-widest">so, you want to travel to</span>
       <h1 className="text-8xl font-bellefair text-white uppercase mt-8">space</h1>
       <span className="w-5/6 text-base text-tropical_blue font-barlow line-clamp-5 text-center leading-7 mt-8">
         Let’s face it; if you want to go to space, you might as well genuinely go to outer space 
