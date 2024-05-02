d2cd4e89b87fd2f4aa5e47f4b4d6031e02fb9b8f
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-02 08:38:01 -0300

    add margin top on destination page

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -11,7 +11,7 @@ function PickDestination() {
       <TabsContent className='flex flex-col items-center gap-6' value={value}>
         <div className='flex flex-col items-center'>
           <h1 className='text-6xl md:text-7xl font-bellefair text-white'>{title}</h1>
-          <span className='w-5/6 text-base md:text-lg text-tropical_blue font-barlow line-clamp-5 text-center mt-1'>
+          <span className='w-5/6 text-base md:text-lg text-tropical_blue font-barlow line-clamp-5 text-center mt-1 md:mt-5'>
             {description}
           </span>
         </div>
