47df44de1793d8e660db035b3df83c9fe1d05bfd
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-02 19:55:51 -0300

    add text base to technology termilogy on md and ahead

--- src/components/utils/TechnologyCarousel.jsx
+++ src/components/utils/TechnologyCarousel.jsx
@@ -34,7 +34,7 @@ const EmblaCarousel = (props) => {
             <div className={`${styles.embla__slide} flex flex-col items-center`} key={technology}>
               <img className='h-56 md:h-80 md:w-full' src={technology.src} alt={technology.name} />
               <div className='flex flex-col items-center mt-20'>
-                <span className='font-barlow text-sm text-tropical_blue tracking-widest uppercase mb-1'>THE TERMINOLOGY…</span>
+                <span className='font-barlow text-sm md:text-base text-tropical_blue tracking-widest uppercase mb-1'>THE TERMINOLOGY…</span>
                 <span className='font-bellefair text-2xl text-white uppercase'>{technology.name}</span>
                 <span className='w-5/6 font-barlow text-base text-center text-tropical_blue mt-3'>{technology.description}</span>
               </div>
