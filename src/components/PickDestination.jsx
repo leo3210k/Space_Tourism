7f993b40d57b0aa7e2d8842e5e5c0a70ef9433f6
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-03 16:49:53 -0300

    remove destination title margin left on lg and ahead

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -32,7 +32,7 @@ function PickDestination() {
 
   return (
     <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8 lg:mt-12 lg:mx-28'>
-      <div className='flex md:self-start gap-4 text-base md:text-xl lg:text-3xl font-barlow tracking-widest md:ml-10'>
+      <div className='flex md:self-start gap-4 text-base md:text-xl lg:text-3xl font-barlow tracking-widest md:ml-10 lg:ml-0'>
         <span className='font-bold text-white/25'>01</span>
         <span className='text-white uppercase'>pick your destination</span>
       </div>
