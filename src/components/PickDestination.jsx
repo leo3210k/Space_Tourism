61d71e8b91e50d2882070607ad172c5b2fc4a886
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-03 16:46:12 -0300

    add margin top and horizontal on destination content on lg and ahead

--- src/components/PickDestination.jsx
+++ src/components/PickDestination.jsx
@@ -31,7 +31,7 @@ function PickDestination() {
   }
 
   return (
-    <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8'>
+    <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8 lg:mt-10 lg:mx-16'>
       <div className='flex md:self-start gap-4 text-base md:text-xl lg:text-3xl font-barlow tracking-widest md:ml-10'>
         <span className='font-bold text-white/25'>01</span>
         <span className='text-white uppercase'>pick your destination</span>
