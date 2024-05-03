3e3663ff2a1d2a748f1d447db018db999c429e85
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-02 21:05:47 -0300

    add relative div to header list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,12 +15,14 @@ function Header() {
       <img src={logo} className='ml-6 mt-6 md:ml-10 md:mt-0 self-center' alt="logo" />
       <MenuToggle isSideMenuOpen={isSideMenuOpen} toggle={() => setIsSideMenuOpen(!isSideMenuOpen)} />
       <SideMenu isSideMenuOpen={isSideMenuOpen}/>
-      <ol className='hidden md:flex gap-x-10 font-barlow text-sm text-white bg-dusty_gray/[.04] tracking-widest uppercase px-10'>
-        <CustomLink name="home" path="/"/>
-        <CustomLink name="destination" path="/destination"/>
-        <CustomLink name="crew" path="/crew"/>
-        <CustomLink name="technology" path="/technology"/>
-      </ol>
+      <div className='hidden relative md:flex'>
+        <ol className='flex gap-x-10 font-barlow text-sm text-white bg-dusty_gray/[.04] tracking-widest uppercase px-10'>
+          <CustomLink name="home" path="/"/>
+          <CustomLink name="destination" path="/destination"/>
+          <CustomLink name="crew" path="/crew"/>
+          <CustomLink name="technology" path="/technology"/>
+        </ol>
+      </div>
     </header>
   );
 }
