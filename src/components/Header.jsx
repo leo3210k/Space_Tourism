08ca300a9648861615bd223bb122b2f6a7500a1b
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-03 16:49:23 -0300

    create ilusion of hr on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,11 @@ function Header() {
   return (
     <header className="relative flex justify-between lg:pt-8">
       <img src={logo} className='ml-6 mt-6 md:ml-10 md:mt-0 lg:ml-14 self-center' alt="logo" />
+      <hr className='hidden lg:block absolute w-6/12 border-white/40 top-[5.25rem] left-40'/>
       <MenuToggle isSideMenuOpen={isSideMenuOpen} toggle={() => setIsSideMenuOpen(!isSideMenuOpen)} />
       <SideMenu isSideMenuOpen={isSideMenuOpen}/>
       <div className='hidden relative lg:w-7/12 md:flex bg-dusty_gray/[.04] lg:backdrop-blur-2xl'>
-        <hr className='hidden lg:block absolute w-6/12 border-white/40 top-1/2 -left-[24rem]'/>
+        <hr className='hidden lg:block absolute w-1/12 border-white/40 top-1/2 end-auto'/>
         <ol className='flex gap-x-10 font-barlow text-sm lg:text-base text-white tracking-widest uppercase px-10 lg:pl-32'>
           <CustomLink name="home" path="/" number="00"/>
           <CustomLink name="destination" path="/destination" number="01"/>
