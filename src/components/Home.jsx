cca97e679926438ae93eb0489ba2e0ea2af16958
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-03 08:53:44 -0300

    fix margin top removed from home description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <div className='flex flex-col items-center'>
         <span className="text-base md:text-xl lg:text-3xl font-barlow text-tropical_blue uppercase tracking-widest">so, you want to travel to</span>
         <h1 className="text-8xl md:text-[9.375rem] font-bellefair text-white uppercase mt-8">space</h1>
-        <span className="w-5/6 md:w-3/6 text-base md:text-lg text-tropical_blue font-barlow line-clamp-5 text-center md:leading-7 mt-6">
+        <span className="w-5/6 md:w-3/6 text-base md:text-lg text-tropical_blue font-barlow line-clamp-5 text-center md:leading-7 mt-8 md:mt-6">
           Let’s face it; if you want to go to space, you might as well genuinely go to outer space 
           and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a 
           truly out of this world experience!
