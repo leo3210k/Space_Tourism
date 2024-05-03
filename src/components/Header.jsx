8ee3d3d4c9166570e696fdde027a8c19ae8f2c4a
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-02 21:07:31 -0300

    correct menu header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,9 +15,9 @@ function Header() {
       <img src={logo} className='ml-6 mt-6 md:ml-10 md:mt-0 self-center' alt="logo" />
       <MenuToggle isSideMenuOpen={isSideMenuOpen} toggle={() => setIsSideMenuOpen(!isSideMenuOpen)} />
       <SideMenu isSideMenuOpen={isSideMenuOpen}/>
-      <div className='hidden relative md:flex backdrop-blur-2xl'>
+      <div className='hidden relative md:flex bg-dusty_gray/[.04] backdrop-blur-2xl'>
         <hr className='hidden lg:block absolute w-11/12 border-dusty_gray top-1/2 -left-96'/>
-        <ol className='flex gap-x-10 font-barlow text-sm text-white bg-dusty_gray/[.04] tracking-widest uppercase px-10'>
+        <ol className='flex gap-x-10 font-barlow text-sm text-white tracking-widest uppercase px-10'>
           <CustomLink name="home" path="/"/>
           <CustomLink name="destination" path="/destination"/>
           <CustomLink name="crew" path="/crew"/>
