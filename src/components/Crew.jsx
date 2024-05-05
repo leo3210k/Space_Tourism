17c5f5f64db5193acf7d357b481e788bb6fd913c
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-05 12:34:29 -0300

    add margin top on lg and ahead

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -32,7 +32,7 @@ function Crew() {
   ]
 
   return (
-    <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8'>
+    <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8 lg:mt-12'>
       <div className='flex md:self-start gap-4 text-base md:text-xl font-barlow tracking-widest md:ml-10'>
         <span className='font-bold text-white/25'>02</span>
         <span className='text-white uppercase'>meet your crew</span>
