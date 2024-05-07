0d483a36b7d38fbb6e2c6befa9826dcb85beb277
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-07 12:41:00 -0300

    add self end to crew image

--- src/components/utils/CrewCarousel.jsx
+++ src/components/utils/CrewCarousel.jsx
@@ -32,7 +32,7 @@ const EmblaCarousel = (props) => {
         <div className={`${styles.embla__container}`}>
           {slides.map((person) => (
             <div className={`${styles.embla__slide} flex flex-col items-center lg:items-start`} key={person}>
-              <img className='md:absolute h-56 md:h-[33.25rem] md:-bottom-[40.5rem]' src={person.src} alt={person.name} />
+              <img className='md:absolute h-56 lg:self-end md:h-[33.25rem] md:-bottom-[40.5rem] lg:-bottom-[14rem]' src={person.src} alt={person.name} />
               <hr className='md:hidden w-10/12 border-bright_gray'/>
               <div className='lg:w-7/12 flex flex-col items-center lg:items-start lg:gap-2 mt-16 md:mt-0 lg:mt-32'>
                 <span className='font-bellefair text-base md:text-2xl lg:text-4xl text-white opacity-50 uppercase mb-1'>{person.title}</span>
