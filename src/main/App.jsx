d68398d56709ee38cada302e68a3db868466d049
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-09 16:53:31 -0300

    merge modifications

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,28 +1,38 @@
 import './App.css';
-
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
+const mobileWidthTreshold = 1024;
+
 function App() {
-  let location = useLocation();
-  const [currentLocation, setCurrentLocation] = useState('home-mobile');
+  const location = useLocation();
+  const [currentLocationClass, setCurrentLocationClass] = useState('');
 
   useEffect(() => {
-    if(location.pathname === '/') {
-      setCurrentLocation('home-mobile')
-    } else if(location.pathname === '/destination') {
-      setCurrentLocation('destination-mobile')
-    } else if(location.pathname === '/crew') {
-      setCurrentLocation('crew-mobile')
-    } else if(location.pathname === '/technology') {
-      setCurrentLocation('technology-mobile')
-    }
-  }, [location.pathname])
+    const determineBackgroundClass = () => {
+      const { pathname } = location;
+      const isMobile = window.innerWidth < mobileWidthTreshold;
+
+      if (pathname === '/') {
+        return isMobile ? 'bg-home-mobile' : 'bg-home-desktop';
+      } else if (pathname === '/destination') {
+        return isMobile ? 'bg-destination-mobile' : 'bg-destination-desktop';
+      } else if (pathname === '/crew' || pathname === '/crew/') {
+        return isMobile ? 'bg-crew-mobile' : 'bg-crew-desktop';
+      } else if (pathname === '/technology' || pathname === '/technology/') {
+        return isMobile ? 'bg-technology-mobile' : 'bg-technology-desktop';
+      }
+
+      return '';
+    };
+
+    setCurrentLocationClass(determineBackgroundClass());
+  }, [location]);
 
   return (
-    <div className={`app min-h-screen bg-home-desktop`}>
+    <div className={`app min-h-screen ${currentLocationClass}`}>
       <Header />
       <Outlet />
     </div>
