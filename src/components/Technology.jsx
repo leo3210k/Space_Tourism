85d27dfa1c83f5fbd4c4b594ca5afec01e7ef41d
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-21 08:57:21 -0300

    add margin bottom on medium devices

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -26,7 +26,7 @@ function Technology() {
   ]
 
   return (
-    <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8 lg:mt-12 lg:ml-24'>
+    <div className='flex flex-col justify-center items-center gap-6 md:gap-12 mt-5 md:mt-8 md:mb-6 lg:mt-12 lg:ml-24 lg:mb-0'>
       <div className='flex md:self-start gap-4 text-base md:text-xl lg:text-3xl font-barlow tracking-widest md:ml-10 lg:ml-0'>
         <span className='font-bold text-white/25'>03</span>
         <span className='text-white uppercase'>SPACE LAUNCH 101</span>
